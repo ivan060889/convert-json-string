a5aeac8b384c35e11cb1d1be1b82c6afc683eafa
ivan_moran <leo.wimc@gmail.com>
2022-07-29 19:06:10 -0500

    initial commnit

---
+++ src/main/java/in/reqres/api/convert/json/string/ConvertJsonStringApplication.java
@@ -0,0 +1,13 @@
+package in.reqres.api.convert.json.string;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertJsonStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertJsonStringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/reqres/api/convert/json/string/ConvertJsonStringApplicationTests.java
@@ -0,0 +1,13 @@
+package in.reqres.api.convert.json.string;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertJsonStringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
